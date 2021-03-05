ebf45b5efb478bfffce98bf7ea763cbb99caac33
Ubuntu <jeffreyschuitema@outlook.com>
2021-03-05 17:55:40 +0000

    added content to many views, fixed some controller errors

---
+++ spec/views/semesters/index.html.erb_spec.rb
@@ -0,0 +1,14 @@
+require 'rails_helper'
+
+RSpec.describe "semesters/index", type: :view do
+  before(:each) do
+    assign(:semesters, [
+      Semester.create!(),
+      Semester.create!()
+    ])
+  end
+
+  it "renders a list of semesters" do
+    render
+  end
+end
