3df82be5daf8df349b3b333ffdea4c99fac93fb6
Ubuntu <aelgazar@citadel.edu>
2021-03-15 07:34:05 +0000

    Added CanCan for User abilities

---
+++ app/models/ability.rb
@@ -0,0 +1,42 @@
+class Ability
+  include CanCan::Ability
+#if you are going to use these in the show.html.erb you need to put above the <%link_to 
+#<%if can? :actionperform, @model you want to perform it on checking users ability
+
+#also in the controller.rb of function like def edit you would have to put at the bottom
+#of the method unauthorized! if cannot? 
+  def initialize(user)
+  can :read, :all 
+  
+  end 
+end
+
+  
+  
+  
+    # Define abilities for the passed in user here. For example:
+    #
+    #   user ||= User.new # guest user (not logged in)
+    #   if user.admin?
+    #     can :manage, :all
+    #   else
+    #     can :read, :all
+    #   end
+    #
+    # The first argument to `can` is the action you are giving the user 
+    # permission to do.
+    # If you pass :manage it will apply to every action. Other common actions
+    # here are :read, :create, :update and :destroy.
+    #
+    # The second argument is the resource the user can perform the action on. 
+    # If you pass :all it will apply to every resource. Otherwise pass a Ruby
+    # class of the resource.
+    #
+    # The third argument is an optional hash of conditions to further filter the
+    # objects.
+    # For example, here the user can only update published articles.
+    #
+    #   can :update, Article, :published => true
+    #
+    # See the wiki for details:
+    # https://github.com/ryanb/cancan/wiki/Defining-Abilities
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,21 @@
 Rails.application.routes.draw do
   
+  #these constraints are for when we figure out the associations with users to students, programdirectors, professors/advisors
+  
+   # constraints Clearance::Constraints::SignedIn.new { |user| user.programdirectors? } do
+   # root to: "programdirectors/dashboards#show", as: :programdirectors_root
+  #end
+
+ # constraints Clearance::Constraints::SignedIn.new { |user| user.professors? } do
+  #  root to: "professors/dashboards#show", as: :professors_root
+ # end
+
+
+ # constraints Clearance::Constraints::SignedIn.new { |user| user.students? } do
+  #  root to: "students/dashboards#show", as: :students_root
+ # end
+
+  
   resources :comments
   resources :discussions
   root "courses#index"
