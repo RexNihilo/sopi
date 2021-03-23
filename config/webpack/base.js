4888347055af41bd21d5ad8aeef384da9e1e4392
Ubuntu <JeffreySchuitema@outlook.com>
2021-03-23 04:54:25 +0000

    9

--- config/webpack/base.js
+++ config/webpack/base.js
@@ -1,4 +1,10 @@
 const { webpackConfig } = require('@rails/webpacker')
 
+const VueLoaderPlugin = require('vue-loader/lib/plugin')
+environment.plugins.append(
+  'VueLoaderPlugin',
+  new VueLoaderPlugin()
+)
+
 module.exports = webpackConfig
 
