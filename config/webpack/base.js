f3a3b02d05f95726c652c888966101a405499309
Ubuntu <JeffreySchuitema@outlook.com>
2021-03-23 04:33:41 +0000

    7

--- config/webpack/base.js
+++ config/webpack/base.js
@@ -1,3 +1,5 @@
 const { webpackConfig } = require('@rails/webpacker')
 
 module.exports = webpackConfig
+
+{ test: /\.vue$/, include: /src/, loader: 'vue-loader', options: { loaders: { js: 'awesome-typescript-loader?silent=true' } } }
